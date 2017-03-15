336bab1fed7d55d8ce171b28187c22e7da7d7f87
Hectorlobo10000 <hector.lobo10000@gmail.com>
2017-03-15 15:11:35 -0600

    response.json update

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 5000);
 
 app.get('/', function(resquest, response){
-	response.send("Prados House Web Service");
+	response.json({message:'Prados House Web Service'});
 });
 
 app.listen(app.get('port'), function(){
